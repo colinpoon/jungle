fbf191b2ade54e67381b6561e3245a45648f543b
Colin Poon <me@colinpoon.com>
2021-11-17 02:41:54 -0800

    tests for products and re modeling database 4 user

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -10,7 +10,33 @@ RSpec.describe Product, type: :model do
       expect(@product.errors.full_messages).not_to include("Field can't be blank")
     end
 
+    it 'Does not save a new product if name field is empty' do
+      @category = Category.new(name: "test")
+      @product = Product.new(name: nil, price_cents: 1500, quantity: 17, category: @category)
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'Does not save a new product if quantity field is empty' do
+      @category = Category.new(name: "test")
+      @product = Product.new(name: "Sponge Bob Popsicle", price_cents: 1500, quantity: nil, category: @category)
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it 'Does not save a new product if price field is empty' do
+      @category = Category.new(name: "test")
+      @product = Product.new(name: "Sponge Bob Popsicle", price_cents: nil, quantity: 17, category: @category)
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it 'Does not save a new product if category is empty' do
+      @category = Category.new(name: "test")
+      @product = Product.new(name: "Sponge Bob Popsicle", price_cents: 1500, quantity: 17, category: nil)
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end
 
   end
 end
-
