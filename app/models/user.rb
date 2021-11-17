595a507b9e6db45371812f11de38cef7e1ebe766
Colin Poon <me@colinpoon.com>
2021-11-17 15:52:52 -0800

    added conditions to users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, length: {minimum: 6}
+
+  def self.authenticate_with_credentials(email, password)
+    existing_email = email.downcase.strip
+
+    @user = User.find_by_email(existing_email)
+    if @user && @user.authenticate(password) #authenticate is an instance method by has_secure_password gem
+      @user
+    else
+      nil
+    end
+  end
 end
