a6d874b0dbc7615c68f51c862be08e66193c0ee0
Colin Poon <me@colinpoon.com>
2021-11-17 15:56:38 -0800

    user-specs test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+    describe 'Validations' do
+      it 'Creates a new user if all validations are true' do
+        @user = User.new(name: "hihi", email: "hihi@gmail.com", password: "123", password_confirmation: "123")
+        @user.valid?
+        expect(@user.errors.full_messages).not_to include("Field can't be blank")
+      end
+
 end
